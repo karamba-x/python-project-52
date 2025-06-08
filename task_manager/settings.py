07fb1d61ea9ea9f480cc2a198f22fede58f6f0fd
karamba <kadykov.x@gmail.com>
2025-06-08 18:22:43 +0900

    fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -148,3 +148,4 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
